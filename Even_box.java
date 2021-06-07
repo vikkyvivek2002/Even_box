7182c2de04decd059d4441ab52d97cc3b9e73b6e
vikkyvivek2002 <84318895+vikkyvivek2002@users.noreply.github.com>
2021-06-08 00:04:08 +0530

    Add files via upload

---
+++ Even_box.java
@@ -0,0 +1,16 @@
+class Even_box
+{
+public static void main(String[] args)
+{
+ int n = 2;
+ for(int i=1; i<=5; i++)
+{
+  for(int j=1; j<=5; j++)
+{
+System.out.printf("%3d",n);
+n = n+2;
+}
+System.out.println();
+}
+}
+}
